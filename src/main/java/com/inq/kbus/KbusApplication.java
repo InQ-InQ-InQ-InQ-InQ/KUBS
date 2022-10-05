78b5e092580948dca3865c6ff79f26038c3d9e51
eunchannam <namhm23@gmail.com>
2022-10-05 15:20:40 +0900

    initial commit

---
+++ src/main/java/com/inq/kbus/KbusApplication.java
@@ -0,0 +1,13 @@
+package com.inq.kbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inq/kbus/KbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inq.kbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
