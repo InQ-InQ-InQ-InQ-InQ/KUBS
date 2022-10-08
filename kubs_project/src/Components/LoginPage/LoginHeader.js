acfe1b9dfb1e73a555c0ab8fe7158bc2d61855b7
SWARVY <swarvy0826@naver.com>
2022-10-08 21:43:42 +0900

    221008 테스트용 컴포넌트 생성

---
+++ kubs_project/src/Components/LoginPage/LoginHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginHeader = () => {
+    return (
+        <header>
+            <div>
+                <h1>LoginPage</h1>
+            </div>
+        </header>
+    );
+};
+
+export default LoginHeader;
