191dc8aa2d242ddf1b01534118755e62f16d0817
SWARVY <swarvy0826@naver.com>
2022-11-01 20:26:52 +0900

    d1101 commit

---
+++ kubs_project/src/containers/RegisterContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginComponent from "../components/LoginComponent";
+
+const RegisterContainer = () => {
+    return <LoginComponent />
+};
+
+export default RegisterContainer;
