d0841ebb251ca79296baa256066287f5973d3609
SWARVY <swarvy0826@naver.com>
2022-11-16 23:11:28 +0900

    feat : 비동기 액션 생성에 따른 서버 코드 추가

--- kubs_project/server.js
+++ kubs_project/server.js
@@ -1,28 +1,33 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 app.listen(8080, function () {
-  console.log('listening on 8080')
-}); 
-
-app.use(express.static(path.join(__dirname, 'build')));
-app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-
-app.get('*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/build/index.html'));
+  console.log("listening on 8080");
 });
 
-app.post('/api/login', function (request, response) {
-    console.log(request.body)
-    response.send(request.body);
-})
+app.use(express.static(path.join(__dirname, "build")));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.post('/api/members', function (request, response) {
-    console.log(request.body)
-    response.send(request.body);
-})
+app.get("*", function (request, response) {
+  response.sendFile(path.join(__dirname, "/build/index.html"));
+});
+
+app.post("/api/login", function (request, response) {
+  console.log(request.body);
+  response.send(request.body);
+});
+
+app.post("/api/members", function (request, response) {
+  console.log(request.body);
+  response.send(request.body);
+});
+
+app.post("/api/find", function (request, response) {
+  console.log(request.body);
+  response.send(request.body);
+});
