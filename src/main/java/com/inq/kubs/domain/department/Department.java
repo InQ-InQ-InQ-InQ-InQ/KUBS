9b3c3fc702515bdf8a918199adbb6243b6a454ab
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2022-11-17 14:23:45 +0900

    QW-13 %과이름% 형식으로 검색구현 (#5)

--- src/main/java/com/inq/kubs/domain/department/Department.java
+++ src/main/java/com/inq/kubs/domain/department/Department.java
@@ -4,10 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -18,5 +15,5 @@ public class Department {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String dName;
+    private String departmentName;
 }
