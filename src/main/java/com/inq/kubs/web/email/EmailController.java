5a21a53d61ae97a3c872c777351d682d847592f6
eunchannam <namhm23@gmail.com>
2022-11-17 15:34:57 +0900

    메일로직 패키지구조 변경 + 공통로직 뽑기

---
+++ src/main/java/com/inq/kubs/web/email/EmailController.java
@@ -0,0 +1,48 @@
+package com.inq.kubs.web.email;
+
+import com.inq.kubs.domain.email.EmailService;
+import com.inq.kubs.domain.email.MailDto;
+import com.inq.kubs.web.common.consts.SessionConst;
+import com.inq.kubs.web.common.logic.CommonMethod;
+import com.inq.kubs.web.common.response.Success;
+import com.inq.kubs.web.exception.ErrorType;
+import com.inq.kubs.web.exception.KubsException;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @GetMapping("/email/validation")
+    @Operation(summary = "검증 이메일 발송", description = "이메일을 입력받아 해당 이메일로 검증코드가 담긴 메일을 발송한다.")
+    public ResponseEntity<Success> sendValidationMail(@RequestParam String email,
+                                                      HttpServletRequest request) {
+
+        CommonMethod.registerKeyAndSendMail(email, request, emailService);
+
+        return new ResponseEntity<>(new Success(true), HttpStatus.OK);
+    }
+
+    @PostMapping("/email/validation")
+    @Operation(summary = "검증코드 확인", description = "검증코드를 입력받아 올바른 코드인지 확인한다.")
+    public ResponseEntity<Success> validateKey(@RequestParam("key") String inputKey,
+                                               HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+        String key = (String) session.getAttribute(SessionConst.V_KEY);
+
+        if (key.equals(inputKey)) {
+            return new ResponseEntity<>(new Success(true), HttpStatus.OK);
+        } else throw new KubsException(ErrorType.INCONSISTENT_KEY);
+    }
+}
--- src/main/java/com/inq/kubs/web/member/MemberController.java
+++ src/main/java/com/inq/kubs/web/member/MemberController.java
@@ -5,7 +5,9 @@ import com.inq.kubs.domain.email.MailDto;
 import com.inq.kubs.domain.member.dto.request.FindPwRequest;
 import com.inq.kubs.domain.member.service.MemberService;
 import com.inq.kubs.web.common.consts.SessionConst;
+import com.inq.kubs.web.common.logic.CommonMethod;
 import com.inq.kubs.web.common.response.Success;
+import com.inq.kubs.web.email.EmailController;
 import com.inq.kubs.web.exception.ErrorType;
 import com.inq.kubs.web.exception.KubsException;
 import com.inq.kubs.domain.member.dto.request.CreateMemberRequest;
@@ -40,48 +42,13 @@ public class MemberController {
         return new ResponseEntity<>(new SimpleMemberCreatedResponse(true, memberId), headers, HttpStatus.CREATED);
     }
 
-    @GetMapping("/email/validation")
-    @Operation(summary = "검증 이메일 발송", description = "이메일을 입력받아 해당 이메일로 검증코드가 담긴 메일을 발송한다.")
-    public ResponseEntity<Success> sendValidationMail(@RequestParam String email,
-                                                      HttpServletRequest request) {
-
-        registerKeyAndSendMail(email, request);
-
-        return new ResponseEntity<>(new Success(true), HttpStatus.OK);
-    }
-
-    @PostMapping("/email/validation")
-    @Operation(summary = "검증코드 확인", description = "검증코드를 입력받아 올바른 코드인지 확인한다.")
-    public ResponseEntity<Success> validateKey(@RequestParam("key") String inputKey,
-                                               HttpServletRequest request) {
-
-        HttpSession session = request.getSession(false);
-        String key = (String) session.getAttribute(SessionConst.V_KEY);
-
-        if (key.equals(inputKey)) {
-            return new ResponseEntity<>(new Success(true), HttpStatus.OK);
-        } else throw new KubsException(ErrorType.INCONSISTENT_KEY);
-    }
-
     @PostMapping("/pw/find")
     public ResponseEntity<Success> findPwOperation(@ModelAttribute FindPwRequest request,
                                                    HttpServletRequest httpServletRequest) {
 
         memberService.checkFindPwRequest(request);
-        registerKeyAndSendMail(request.getEmail(), httpServletRequest);
+        CommonMethod.registerKeyAndSendMail(request.getEmail(), httpServletRequest, emailService);
 
         return new ResponseEntity<>(new Success(true), HttpStatus.OK);
-    }
-
-    //세션에 인증 키를 등록하고 인증키가 담긴 메일을 전송한다.
-    private void registerKeyAndSendMail(String email, HttpServletRequest request) {
-        int intKey = (int) (Math.random() * 8999 + 1000);
-        String key = String.valueOf(intKey);
-        MailDto mailDto = new MailDto(email, "KUBS_학생인증", key);
-
-        HttpSession session = request.getSession();
-        session.setAttribute(SessionConst.V_KEY, key);
-
-        emailService.sendValidationMail(mailDto);
     }
 }
