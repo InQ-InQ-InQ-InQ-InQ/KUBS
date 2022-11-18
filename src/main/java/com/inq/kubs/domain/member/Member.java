7359d6db10bad43603035141471e89e79a295e1d
Haebin <hbyeon@student.42seoul.kr>
2022-11-18 13:24:59 +0900

    QW-12 상세 예약 조회 구현

--- src/main/java/com/inq/kubs/domain/member/Member.java
+++ src/main/java/com/inq/kubs/domain/member/Member.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-
+    private Long id;
     private String name;
 
     private Long studentId;
--- src/main/java/com/inq/kubs/web/booking/dto/response/DetailBookingResponse.java
+++ src/main/java/com/inq/kubs/web/booking/dto/response/DetailBookingResponse.java
@@ -1,4 +1,5 @@
 package com.inq.kubs.web.booking.dto.response;
+import com.inq.kubs.domain.booking.Booking;
 import lombok.Data;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -6,7 +7,7 @@ import java.time.LocalTime;
 public class DetailBookingResponse {
     //TODO 이렇게 파라미터 다 쪼개서 받으면 파라미터 넘기는것도 힘들고 나중에 수정할때 진짜 노답이라서 여기서는 Booking 을 DetailBookingResponse로 만드는거니까
     //TODO 파라미터로 Booking 을 받아서 하면됩니다. 예시는 SimplePlaceReponse를 보시면 됩니다.
-    public DetailBookingResponse(String name, String placeName, LocalDate date, LocalTime startTime, LocalTime endTime) {
+    public DetailBookingResponse(Booking booking, String name) {
         this.name = name;
         this.placeName = placeName;
         this.date = date;
