0a7b95277dae79892ecd33d251fdaf93355e67a4
SWARVY <swarvy0826@naver.com>
2022-11-25 11:48:17 +0900

    fix : 액션 타입 변경

--- kubs_project/src/thunk/department.js
+++ kubs_project/src/thunk/department.js
@@ -1,13 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const department = createAsyncThunk("user/FIND_PW", async (data) => {
-  console.log(data);
-  const result = await axios.post("http://localhost:8080/api/find", {
-    keyword: data.keyword,
-  });
+export const department = createAsyncThunk(
+  "user/FIND_DEPARTMENT",
+  async (data) => {
+    console.log(data);
+    const result = await axios.post("http://localhost:8080/api/find", {
+      keyword: data.keyword,
+    });
 
-  console.log("result: ", result);
+    console.log("result: ", result);
 
-  return result;
-});
+    return result;
+  }
+);
