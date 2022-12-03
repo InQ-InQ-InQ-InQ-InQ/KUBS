6a97e807943b33ed2ca2994695ff45a9832fedd8
SWARVY <swarvy0826@naver.com>
2022-12-03 20:33:30 +0900

    feat : 비밀번호 찾기 기능 구현

--- kubs_project/src/components/FindComponent.js
+++ kubs_project/src/components/FindComponent.js
@@ -1,42 +1,123 @@
-import { Button, Form, Modal } from "react-bootstrap";
+import { Button, Form, Modal, InputGroup } from "react-bootstrap";
 import "../styles/FindStyle.css";
 
 const FindComponent = ({
   onPWFindIdHandler,
+  onPWFindPhoneNumberHandler,
   onPWFindEmailHandler,
   onPWFindSubmitHandler,
+  onChangePasswordHandler,
+  onChangePasswordConfirmHandler,
+  onChangePasswordSubmitHandler,
   handleClose,
   show,
+  onValidateEmailHandler,
+  onValidateHandler,
+  onValidateConfirmHandler,
+  sendInvisible,
+  verifyInvisible,
+  pwFindInvisible,
 }) => {
   return (
-    <Modal size="lg" centered show={show} onHide={handleClose}>
+    <Modal centered show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>아이디/비밀번호 찾기</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form className="findSection" onSubmit={onPWFindSubmitHandler}>
-          <Form.Group className="mb-3">
-            <Form.Label>비밀번호 찾기</Form.Label>
+        {!pwFindInvisible ? (
+          <Form className="findSection" onSubmit={onPWFindSubmitHandler}>
             <Form.Group className="mb-3">
+              <Form.Label>비밀번호 찾기</Form.Label>
               <Form.Control
+                className="mb-3"
                 type="text"
                 placeholder="아이디"
                 onChange={onPWFindIdHandler}
               />
+              <Form.Control
+                className="mb-3"
+                type="text"
+                placeholder="핸드폰번호"
+                onChange={onPWFindPhoneNumberHandler}
+              />
+              <InputGroup className="mb-3">
+                <Form.Control
+                  placeholder="경기대학교 이메일"
+                  aria-label="경기대학교 이메일"
+                  onChange={onPWFindEmailHandler}
+                />
+                {!sendInvisible ? (
+                  <Button
+                    variant="outline-secondary"
+                    id="button-addon2"
+                    onClick={onValidateEmailHandler}
+                  >
+                    전송
+                  </Button>
+                ) : (
+                  <Button
+                    variant="outline-secondary"
+                    id="button-addon2"
+                    disabled
+                  >
+                    전송
+                  </Button>
+                )}
+              </InputGroup>
+              {sendInvisible ? (
+                <InputGroup className="mb-3">
+                  <Form.Control
+                    placeholder="인증 코드"
+                    aria-label="인증 코드"
+                    onChange={onValidateHandler}
+                  />
+                  {!verifyInvisible ? (
+                    <Button
+                      variant="outline-secondary"
+                      id="button-addon2"
+                      onClick={onValidateConfirmHandler}
+                    >
+                      인증
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="outline-secondary"
+                      id="button-addon2"
+                      disabled
+                    >
+                      인증
+                    </Button>
+                  )}
+                </InputGroup>
+              ) : (
+                <></>
+              )}
             </Form.Group>
-          </Form.Group>
-          <Form.Control
-            type="email"
-            placeholder="KGU 이메일"
-            onChange={onPWFindEmailHandler}
-          />
-          <Form.Text className="text-muted">
-            가입했을 때 사용한 KGU 이메일을 입력해주세요.
-          </Form.Text>
-          <Button className="buttonSection" variant="primary" type="submit">
-            찾기
-          </Button>
-        </Form>
+            <Button className="buttonSection" variant="primary" type="submit">
+              찾기
+            </Button>
+          </Form>
+        ) : (
+          <Form onSubmit={onChangePasswordSubmitHandler}>
+            <Form.Group>
+              <Form.Control
+                className="mb-3"
+                type="password"
+                placeholder="새 비밀번호"
+                onChange={onChangePasswordHandler}
+              />
+              <Form.Control
+                className="mb-3"
+                type="password"
+                placeholder="새 비밀번호 확인"
+                onChange={onChangePasswordConfirmHandler}
+              />
+            </Form.Group>
+            <Button className="buttonSection" variant="primary" type="submit">
+              변경
+            </Button>
+          </Form>
+        )}
       </Modal.Body>
     </Modal>
   );
--- kubs_project/src/containers/LoginContainer.js
+++ kubs_project/src/containers/LoginContainer.js
@@ -4,15 +4,26 @@ import LoginComponent from "../components/LoginComponent";
 import FindComponent from "../components/FindComponent";
 import { login } from "../thunk/login";
 import { findPW } from "../thunk/findPW";
+import { changePW } from "../thunk/changePW";
+import { postValidation } from "../thunk/postValidation";
+import { getValidation } from "../thunk/getValidation";
+import { useNavigate } from "react-router-dom";
 
 const LoginContainer = () => {
   const dispatch = useDispatch();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [pwFindId, setPWFindId] = useState("");
+  const [pwFindPhoneNumber, setPWFindPhoneNumber] = useState("");
   const [pwFindEmail, setPWFindEmail] = useState("");
+  const [pwFindInvisible, setPWFindInvisible] = useState("");
+  const [changePassword, setChangePassword] = useState("");
+  const [changePasswordConfirm, setChangePasswordConfirm] = useState("");
+  const [sendInvisible, setSendInvisible] = useState(false);
+  const [verifyInvisible, setVerifyInvisible] = useState(false);
+  const [validate, setValidate] = useState("");
   const [show, setShow] = useState(false);
 
   const onUsernameHandler = (e) => {
@@ -27,13 +38,55 @@ const LoginContainer = () => {
     setPWFindId(e.currentTarget.value);
   };
 
+  const onPWFindPhoneNumberHandler = (e) => {
+    setPWFindPhoneNumber(e.currentTarget.value);
+  };
+
   const onPWFindEmailHandler = (e) => {
     setPWFindEmail(e.currentTarget.value);
   };
 
-  const handleClose = () => setShow(false);
+  const onChangePasswordHandler = (e) => {
+    setChangePassword(e.currentTarget.value);
+  };
+
+  const onChangePasswordConfirmHandler = (e) => {
+    setChangePasswordConfirm(e.currentTarget.value);
+  };
+
+  const handleClose = () => {
+    setSendInvisible(false);
+    setVerifyInvisible(false);
+    setPWFindInvisible(false);
+    setShow(false);
+  };
 
   const handleShow = () => setShow(true);
+
+  const onValidateHandler = (e) => {
+    setValidate(e.currentTarget.value);
+  };
+
+  const onValidateEmailHandler = () => {
+    setSendInvisible(true);
+    let body = {
+      email: pwFindEmail,
+    };
+
+    dispatch(postValidation(body));
+  };
+
+  const onValidateConfirmHandler = () => {
+    let body = {
+      validate: validate,
+    };
+
+    dispatch(getValidation(body)).then((res) => {
+      console.log(res);
+      if (res.payload.data.success) setVerifyInvisible(true);
+      else return alert("인증 코드가 일치하지 않습니다!");
+    });
+  };
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
@@ -42,17 +95,53 @@ const LoginContainer = () => {
       password: password,
     };
 
-    dispatch(login(body));
+    dispatch(login(body)).then((res) => {
+      console.log(res);
+      if (res.payload.data.success) navigate("/main");
+      else return alert("회원가입에 실패하였습니다!");
+    });
   };
 
   const onPWFindSubmitHandler = (e) => {
     e.preventDefault();
     let body = {
       pwFindId: pwFindId,
+      pwFindPhoneNumber: pwFindPhoneNumber,
       pwFindEmail: pwFindEmail,
     };
 
-    dispatch(findPW(body));
+    dispatch(findPW(body)).then((res) => {
+      console.log(res);
+      if (res.payload.data.success) {
+        setPWFindInvisible(true);
+        setPWFindPhoneNumber("");
+        setPWFindEmail("");
+      } else return alert("인증 코드가 일치하지 않습니다!");
+    });
+  };
+
+  const onChangePasswordSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (changePassword !== changePasswordConfirm)
+      return alert("새 비밀번호와 비밀번호 확인이 일치하지 않습니다!");
+
+    if (!verifyInvisible) return alert("이메일 인증을 완료하여야 합니다!");
+
+    let body = {
+      pwFindId: pwFindId,
+      changePassword: changePassword,
+      changePasswordConfirm: changePasswordConfirm,
+    };
+
+    dispatch(changePW(body)).then((res) => {
+      if (res.payload.data.success) {
+        setShow(false);
+        setPWFindId("");
+        setChangePassword("");
+        setChangePasswordConfirm("");
+      } else return alert("비밀번호 변경이 실패했습니다!");
+    });
   };
 
   return (
@@ -65,10 +154,20 @@ const LoginContainer = () => {
       />
       <FindComponent
         onPWFindIdHandler={onPWFindIdHandler}
+        onPWFindPhoneNumberHandler={onPWFindPhoneNumberHandler}
         onPWFindEmailHandler={onPWFindEmailHandler}
         onPWFindSubmitHandler={onPWFindSubmitHandler}
+        onChangePasswordHandler={onChangePasswordHandler}
+        onChangePasswordConfirmHandler={onChangePasswordConfirmHandler}
+        onChangePasswordSubmitHandler={onChangePasswordSubmitHandler}
         handleClose={handleClose}
         show={show}
+        onValidateEmailHandler={onValidateEmailHandler}
+        onValidateHandler={onValidateHandler}
+        onValidateConfirmHandler={onValidateConfirmHandler}
+        sendInvisible={sendInvisible}
+        verifyInvisible={verifyInvisible}
+        pwFindInvisible={pwFindInvisible}
       />
     </>
   );
