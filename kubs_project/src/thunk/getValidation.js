e2a02b8454568069302baeb70317b04a4187d48e
SWARVY <swarvy0826@naver.com>
2022-12-03 20:30:57 +0900

    feat : 이메일 메일 발송, 인증 비동기 액션 생성

---
+++ kubs_project/src/thunk/getValidation.js
@@ -0,0 +1,18 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getValidation = createAsyncThunk(
+  "register/GET_VALIDATION",
+  async (data) => {
+    console.log(data);
+    const result = await axios.get("/api/email/validation", {
+      params: {
+        key: data.validate,
+      },
+    });
+
+    console.log("result: ", result);
+
+    return result;
+  }
+);
---
+++ kubs_project/src/thunk/postValidation.js
@@ -0,0 +1,22 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const postValidation = createAsyncThunk(
+  "register/POST_VALIDATION",
+  async (data) => {
+    console.log(data);
+    const result = await axios.post(
+      "/api/email/validation",
+      JSON.stringify({
+        email: data.email,
+      }),
+      {
+        headers: { "Content-Type": `application/json` },
+      }
+    );
+
+    console.log("result: ", result);
+
+    return result;
+  }
+);
