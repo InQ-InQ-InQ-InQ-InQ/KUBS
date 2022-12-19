c13aa785c164ba21758c78d05107934676490e7c
rheefine <rheefine@gmail.com>
2022-12-20 00:22:47 +0900

    test app

--- kubs_project/src/App.js
+++ kubs_project/src/App.js
@@ -4,6 +4,9 @@ import PrivateRoute from './PrivateRoute';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import TestPage from './pages/TestPage';
+import ApplyPage from "./pages/ApplyPage";
+import MainPage from "./pages/MainPage";
+import CompletePage from "./pages/CompletePage";
 
 const App = () => {
   const token = localStorage.getItem('authorized');
@@ -15,6 +18,9 @@ const App = () => {
           <Route path="/" element={<PrivateRoute authenticated={token} component={<LoginPage />} />} />
           <Route path="/register" element={<PrivateRoute authenticated={token} component={<RegisterPage />} />}></Route>
           <Route path="/main" element={<TestPage />} />
+          <Route path="/main" element={<MainPage/>}/>
+          <Route path="/apply" element={<ApplyPage/>}/>
+          <Route path="/complete" element={<CompletePage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
