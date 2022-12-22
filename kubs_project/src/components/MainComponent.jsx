c2479ffae4637e3dd2b34b94c733def1666e4755
SWARVY <swarvy0826@naver.com>
2022-12-22 14:58:37 +0900

    feat : navigate를 사용하여 리프레쉬 방지

--- kubs_project/src/components/MainComponent.jsx
+++ kubs_project/src/components/MainComponent.jsx
@@ -1,60 +1,80 @@
-/* eslint-disable */
 import React, { useState } from 'react';
-import { Container, Nav, Navbar, ToggleButton, ToggleButtonGroup, Modal, Button } from "react-bootstrap";
-// import { Link } from "react-router-dom";
-import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import axios from 'axios';
 
+function MainComponent() {
+  const navigate = useNavigate();
+  const [building, setBuilding] = useState([
+    '1강',
+    '2강',
+    '3강',
+    '예대',
+    '학생관',
+    '복지관',
+    '어울림관',
+    '4강',
+    '5강',
+    '6강',
+    '7강',
+    '8강',
+    '제2공학관',
+    '대운동장',
+    '풋살장',
+    '테니스장',
+  ]);
+  const onMain = (e) => {
+    e.preventDefault();
+    navigate('/main');
+  };
 
-const MainComponent = () => {
+  const onApply = (e) => {
+    e.preventDefault();
+    navigate('/apply');
+  };
 
-  const [building, setBuilding] = useState(["1강", "2강", "3강", "예대", "학생관", "복지관", "어울림관", "4강", "5강", "6강", "7강", "8강", "제2공학관", "대운동장", "풋살장", "테니스장"]);
-
-
-
-
+  const onMypage = (e) => {
+    e.preventDefault();
+    navigate('/mypage');
+  };
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">KUBS</Navbar.Brand>
+          <Navbar.Brand onClick={onMain}>KUBS</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-            </Nav>
-
+            <Nav className="me-auto" />
             <Nav>
-              <Nav.Link href="#mypage">My Page</Nav.Link>
+              <Nav.Link onClick={onApply}>예약</`Nav.Link>
+              <Nav.Link onClick={onMypage}>마이페이지</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-      <div style={{ width: "50%", margin: "auto", marginTop: "20px" }}>
-        {
-          building.map(function (a, i) {
-            return (
-              <>
-                <p onClick={() => {
-                  axios.post('http://localhost:8080/api/login', { building: building[i] })
-                    .then(function (response) {
-                      console.log(response);
-                      // response  
-                    }).catch(function (error) {
-                      console.log(error);
-                      // 오류발생시 실행
-                    })
-                }}>
-                  {building[i]}</p>
-              </>
-            )
-          })
-        }
+      <div style={{ width: '50%', margin: 'auto', marginTop: '20px' }}>
+        {building.map((a, i) => (
+          <p
+            onClick={() => {
+              axios
+                .post('http://localhost:8080/api/login', { building: building[i] })
+                .then((response) => {
+                  console.log(response);
+                  // response
+                })
+                .catch((error) => {
+                  console.log(error);
+                  // 오류발생시 실행
+                });
+            }}
+          >
+            {building[i]}
+          </p>
+        ))}
       </div>
     </>
   );
-};
-
-
-
+}
 
 export default MainComponent;
--- kubs_project/src/components/MypageComponent.jsx
+++ kubs_project/src/components/MypageComponent.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import MypageItemComponent from './MypageItemComponent';
 import '../styles/MypageStyle.css';
 
 function MypageComponent() {
+  const navigate = useNavigate();
+
+  const onMain = (e) => {
+    e.preventDefault();
+    navigate('/main');
+  };
+
+  const onApply = (e) => {
+    e.preventDefault();
+    navigate('/apply');
+  };
+
   const onLogout = (e) => {
     e.preventDefault();
+    navigate('/');
     localStorage.removeItem('authorized');
   };
 
@@ -14,19 +28,24 @@ function MypageComponent() {
     { time: '1200~1300', room: '6008' },
   ];
 
-  const testUser = { studentId: '201912088', name: '신현호', department: '컴퓨터공학부' };
+  const testUser = {
+    studentId: '201912088',
+    name: '신현호',
+    department: '컴퓨터공학부',
+    email: 'abc@naver.com',
+    phoneNumber: '010-0000-0000',
+  };
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">KUBS</Navbar.Brand>
+          <Navbar.Brand onClick={onMain}>KUBS</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto" />
             <Nav>
-              <Nav.Link href="/" onClick={onLogout}>
-                로그아웃
-              </Nav.Link>
+              <Nav.Link onClick={onApply}>예약</Nav.Link>
+              <Nav.Link onClick={onLogout}>로그아웃</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -37,6 +56,8 @@ function MypageComponent() {
             <div>{testUser.studentId}</div>
             <div>{testUser.name}</div>
             <div>{testUser.department}</div>
+            <div>{testUser.phoneNumber}</div>
+            <div>{testUser.email}</div>
           </div>
           <div>
             <h1>예약 목록</h1>
