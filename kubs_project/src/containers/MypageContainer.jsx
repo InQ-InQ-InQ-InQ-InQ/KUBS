668dc6bc55375e4619f00b9247f86a5236c6b1d1
SWARVY <swarvy0826@naver.com>
2022-12-22 16:16:19 +0900

    feat : mypage Container/page 생성 및 불필요한 페이지 삭제

---
+++ kubs_project/src/containers/MypageContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MypageComponent from '../components/MypageComponent';
+
+function MypageContainer() {
+  return <MypageComponent />;
+}
+
+export default MypageContainer;
---
+++ kubs_project/src/pages/MyinfoPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MypageContainer from '../containers/MypageContainer';
+
+function MyinfoPage() {
+  return <MypageContainer />;
+}
+
+export default MyinfoPage;
