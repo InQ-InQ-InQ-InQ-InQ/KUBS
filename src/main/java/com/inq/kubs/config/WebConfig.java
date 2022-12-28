fcd8e99b536983e96527944af517bec252187637
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2022-12-28 20:49:20 +0900

    [BE] 비로그인 사용자 처리 | -> BE (#15)
    * dev : 로그인 인터셉터 틀 구현
    
    * dev : 비로그인 사용자 에러 정의
    
    * dev : 로그인 인터셉터 구현
    
    * dev : 로그인 인터셉터 등록
    
    * dev : 메인페이지 접근 요청 Controller 작성
    
    Co-authored-by: 남은찬 <eunchan@nam-eunchan-ui-MacBookAir.local>

---
+++ src/main/java/com/inq/kubs/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.inq.kubs.config;
+
+import com.inq.kubs.web.login.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/login", "/api/member", "/api/pw/**",
+                                    "/api/id_check/**", "/api/email/**");
+    }
+}
---
+++ src/main/java/com/inq/kubs/web/login/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,42 @@
+package com.inq.kubs.web.login.interceptor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.inq.kubs.web.common.consts.SessionConst;
+import com.inq.kubs.web.common.response.ErrorResponse;
+import com.inq.kubs.web.exception.ErrorType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String requestURI = request.getRequestURI();
+        log.info("인증 체크 인터셉터 실행 {}", requestURI);
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+            log.info("미인증 사용자 요청");
+
+            ErrorType errorType = ErrorType.NON_LOGGED_IN_MEMBER;
+            ErrorResponse errorResponse = new ErrorResponse(false, errorType.getCode(), errorType.getMessage());
+
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.setStatus(401);
+
+            String resultJson = objectMapper.writeValueAsString(errorResponse);
+            response.getWriter().write(resultJson);
+
+            return false;
+        }
+        return true;
+    }
+}
